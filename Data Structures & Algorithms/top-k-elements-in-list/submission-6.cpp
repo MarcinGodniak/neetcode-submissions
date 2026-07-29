9a762c05399709aaec1f2d29a6523a566a1413a5
MarcinGodniak <marcingodniak@gmail.com>
2026-07-30 00:23:28 +0200

    Add: top-k-elements-in-list - submission-6

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-6.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) 
+    {
+        using Item = std::pair<int, int>;
+        auto cmp = [](const Item& a, const Item& b) {return a.first < b.first; };
+        std::vector<Item> items(2000);
+        for(auto& x: nums) {
+            items[x+1000].first++;;
+            items[x+1000].second = x;
+        }
+
+
+        std::erase_if(items, [](auto& x ) {return x.first == 0;});
+
+        std::priority_queue<Item, std::vector<Item>, decltype(cmp)> heap(cmp, std::move(items));
+
+        vector<int> ret(k);
+        for(int i=0; i < k; i++){
+            ret[i]= heap.top().second;
+            heap.pop();
+        }
+        return ret;
+    }
+};
