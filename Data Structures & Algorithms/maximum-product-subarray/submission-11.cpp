0af9449c05d4a0f7ae85f1442e654ffa8f345bc6
MarcinGodniak <marcingodniak@gmail.com>
2026-08-05 09:25:14 +0200

    Add: maximum-product-subarray - submission-11

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-11.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) 
+    {
+        int max_product = std::numeric_limits<int>::min();
+
+        int l =0;
+        int p =0;
+
+        while(l < nums.size()){
+            int negative_count=0;
+
+            while(p< nums.size()) {
+                if(nums[p] < 0) negative_count++;
+                if(nums[p] == 0) {
+                    max_product = std::max(max_product, 0);
+                    break;
+                }
+                p++;
+            }
+
+            int sum_l = l;
+            int sum_p = p;
+            if(negative_count %2 !=0) {
+                //left
+                int left_accum =1;
+                int i_l = l;
+                for( ;i_l < p; i_l++) {
+                    left_accum *= nums[i_l];
+                    if(nums[i_l] < 0) {
+                        break;
+                    }
+                } 
+
+                //right
+                int right_accum =1;
+                int i_r = p-1;
+                for( ;i_r > l; i_r--) {
+                    right_accum *= nums[i_r];
+                    if(nums[i_r] < 0) {
+                        break;
+                    } 
+                } 
+
+                std::cout<< "left_accum "<< left_accum << "right_accum " << right_accum << std::endl;
+                std::cout<< "l "<< l << " p " << p << " i_l " << i_l << " i_r "<< i_r << std::endl;
+                if(left_accum > right_accum) {
+                    sum_l = i_l+1;
+                    sum_p = p;
+                } else {
+                    sum_l = l;
+                    sum_p = i_r > l? i_r: l+1;
+                }
+            }
+            int accum =1? sum_l<sum_p: 0;
+            for(int i =sum_l; i < sum_p; i++) 
+            {
+                accum *=nums[i];
+            }
+
+            max_product = std::max(max_product, accum);
+            std::cout<< "sum_l "<< sum_l << " sum_p " << sum_p << " max_product " << max_product << std::endl;
+            std::cout<< "l "<< l << " p " << p << " max_product " << max_product << std::endl;
+
+            p++;
+            l=p;
+        }
+
+        return max_product;
+    }
+};
